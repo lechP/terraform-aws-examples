32858488dfbe89e16d7db9c5d245c5de8e2678dc
lech.piechota <lech.piechota@ocado.com>
2025-11-12 20:28:47 +0100

    add a reference to the terraform state

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "lpi-tfstate-3e1989"
+    key            = "sandbox/test/terraform.tfstate"
+    region         = "eu-west-3"
+    dynamodb_table = "tf-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
