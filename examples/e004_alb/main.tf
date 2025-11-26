7037405afdf2bc333cc5ee8b9376d2c22cac451e
lech.piechota <lech.piechota@ocado.com>
2025-11-26 18:46:55 +0100

    004 modify health_check so that it reaches the right place

--- examples/e004_alb/main.tf
+++ examples/e004_alb/main.tf
@@ -107,7 +107,7 @@ resource "aws_lb_target_group" "target_group" {
   vpc_id   = data.aws_vpc.default.id
 
   health_check {
-    path                = "/"
+    path                = "/info.json"
     protocol            = "HTTP"
     matcher             = "200"
     interval            = 30
