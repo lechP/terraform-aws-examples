4e4d1e239d7aefc2863e731b519b4b98a318b5f0
Lech <cardano.p@gmail.com>
2025-11-27 16:37:45 +0100

    Update main.tf
    Enable access to ec2

--- examples/e005_nlb/main.tf
+++ examples/e005_nlb/main.tf
@@ -55,7 +55,7 @@ resource "aws_security_group" "instance_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = module.vpc.public_subnets_cidr_blocks
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
